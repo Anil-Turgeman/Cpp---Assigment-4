7381dc3c9ec1328732a86a9362b89ebf42c2a03e
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-30 05:42:17 +0300

    Create Guesser.hpp

---
+++ Guesser.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+using std::string;
+namespace bullpgia{
+    class Guesser{
+        protected:
+            uint length;
+            string reply;
+        
+        public:
+            virtual string guess() = 0;
+            virtual void startNewGame(uint length){};
+            virtual void learn(string reply){};
+    };
+}
