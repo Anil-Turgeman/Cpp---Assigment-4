0865677af3b09d74776bea610e4261a6077a4653
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-30 05:52:12 +0300

    Create SmartGuesser.hpp

---
+++ SmartGuesser.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "Guesser.hpp"
+#include "calculate.hpp"
+#include <string>
+using std::string;
+
+namespace bullpgia{
+    class SmartGuesser: public bullpgia::Guesser{
+        public:
+            string guess() override;
+            void startNewGame(uint length) override;
+            void learn(string reply) override;
+    };
+}
