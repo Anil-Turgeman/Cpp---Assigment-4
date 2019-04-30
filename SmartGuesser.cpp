74582ba18128938e9403baa2cee66396f6d18f04
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-30 05:52:31 +0300

    Create SmartGuesser.cpp

---
+++ SmartGuesser.cpp
@@ -0,0 +1,15 @@
+#include "SmartGuesser.hpp"
+#include "Guesser.hpp"
+#include <string>
+using std::string;
+
+string bullpgia::SmartGuesser::guess() {
+    string result = "";
+    return result;
+}
+
+void bullpgia::SmartGuesser::startNewGame(uint length){
+}
+
+void bullpgia::SmartGuesser::learn(string reply){
+}
