9f971c24599b18db26f6d7edd07b07f37d1a9e3f
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-30 05:42:41 +0300

    Create Chooser.hpp

---
+++ Chooser.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+using std::string;
+using namespace std;
+namespace bullpgia{
+    class Chooser{
+        private:
+            uint length;
+
+        public:
+            virtual string choose(uint length) = 0;
+    };
+}
