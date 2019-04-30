144bbaed6e7db37d781056964d16368ce6a6a1e5
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-30 05:46:14 +0300

    Create calculate.cpp

---
+++ calculate.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "calculate.hpp"
+using std::string;
+namespace bullpgia {
+    string calculateBullAndPgia(string chooser, string guesser) {
+        string result = "";
+        return result;
+    }
+}
