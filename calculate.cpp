fcd9d554906466edf8304d55267cb718206c49e0
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-06 22:26:14 +0300

    Adding the function implementation

--- calculate.cpp
+++ calculate.cpp
@@ -3,7 +3,21 @@
 using std::string;
 namespace bullpgia {
     string calculateBullAndPgia(string chooser, string guesser) {
-        string result = "";
-        return result;
+        int bull = 0, pgia = 0;
+        int arr[chooser.size()];
+        for(int i = 0; i < chooser.size(); i++){
+            if(chooser[i] == guesser[i]){
+                bull++;
+                arr[i] = -1;
+            }else{
+                size_t found = chooser.find(guesser[i]); 
+                if(found < chooser.size() && arr[found] != -1){
+                    pgia++;
+                    arr[found] = -1;
+                }
+            }
+        }
+        
+        return to_string(bull) + ", " + to_string(pgia);
     }
 }
