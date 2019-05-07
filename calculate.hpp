c44e8ef40fa399fea8a2e708bf987004920fed57
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-07 23:23:32 +0300

    Update calculate.hpp

--- calculate.hpp
+++ calculate.hpp
@@ -2,9 +2,4 @@
 #include <string>
 using namespace std;
 
-namespace bullpgia{
     string calculateBullAndPgia(string choice, string guess);
-    int getNumExactCharMatches(string chosen, string guess);
-    int getNumSimilarChars(string chosen, string guess);
-    int* getCharCounts(string str);
-}
