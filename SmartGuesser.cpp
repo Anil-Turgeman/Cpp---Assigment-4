1b58a3a5dd000af2dee8e2e17ecb489d7b51a1cd
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-07 23:30:54 +0300

    Update SmartGuesser.cpp

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,55 +1,31 @@
+#include <iostream>
+#include <string>
 #include "SmartGuesser.hpp"
-#include <stdlib.h>
-#include <iomanip>
-#include <cmath>
 using namespace std;
 using namespace bullpgia;
 
+    int flag = 0;
+    int candidate = 0;
+    string ans = "";
 
-string SmartGuesser::guess(){
-    list<string>::iterator it = combination.begin();
-    int randomIndex = rand()%combination.size();
-    advance(it, randomIndex);//puts the random 
-    guess = *it; 
-    return guess;
-}
+    void SmartGuesser::learn(string reply){
+        int bulls = (int)(reply[0]);
+        if (bulls - 48 == (flag+1)){
+            flag++;
+            candidate = 0;
+        }
+        ans.replace(flag, 1, to_string(candidate));
+        candidate++;
+    }
 
-void SmartGuesser::learn(string calculateStatus) {
-	for (list<string>::iterator it=combination.begin(); it!=combination.end();){
-		string ans = calculateBullAndPgia(*it, guess);
-		if(calculateStatus!=ans){
-			it = combination.erase(it);
-		}else{
-			it++;
-		}
-	}
-}
+    string SmartGuesser::guess() {
+        return ans;
+    }
 
-void SmartGuesser::startNewGame(uint length){
-	this->length = length;
-	createTheNumberToGuess(length);
-}
-
-void SmartGuesser::createTheNumberToGuess(uint length){
-	string num;
-	int MAX_NUM;
-
-	if(!combination.empty()){
-		combination.clear();
-	}
-
-	for(int i = 0; i < length; i++){
-		num += "9";
-	}
-	
-	MAX_NUM = std::stoi(num);	
-	
-	for(int i=0; i <= MAX_NUM; i++){
-		string curr = to_string(i);
-		ostringstream ss;
-		ss  << setw(length) << setfill('0') << curr;
-		string s = ss.str();
-		combination.push_back(s);
-	}
-	
-}
+    void SmartGuesser::startNewGame(uint length){
+        this->length = length;
+        flag = 0;
+        candidate = 0;
+        ans = "wrong";
+        ans = ans.substr(0,length);
+    }
