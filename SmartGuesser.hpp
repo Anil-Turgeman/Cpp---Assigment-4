1da4509379b6dbf916b512996dd2846d4dddf9e1
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-07 22:51:36 +0300

    Update SmartGuesser.hpp

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -2,12 +2,17 @@
 #include "Guesser.hpp"
 #include "calculate.hpp"
 #include <string>
+#include <iterator>
+#include <list>
 using std::string;
 
 namespace bullpgia{
     class SmartGuesser: public bullpgia::Guesser{
         public:
+            string guess;
+            list<string> combination;
             string guess() override;
+            void createTheNumberToGuess(uint length);
             void startNewGame(uint length) override;
             void learn(string reply) override;
     };
