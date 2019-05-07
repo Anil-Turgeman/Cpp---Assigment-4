7fcdc9135db0c01912eb19c420704b60dcefb873
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-05-07 23:50:01 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -2,22 +2,22 @@
 #include "iostream"
 using namespace std ;
 namespace bullpgia {
-string calculateBullAndPgia(string the_choice,string the_guess){
-        int pgia_numbers = 0;
-        int bull_numbers = 0;
+string calculateBullAndPgia(string choice,string guess){
+        int cows = 0;
+        int bulls = 0;
         int num[10] = {0};
-        for(int i=0; i<the_choice.length(); i++){
-            if(the_guess[i]==the_choice[i]){
-                bull_numbers++;
+        for(int i = 0; i < choice.length(); i++){
+            if(guess[i] == choice[i]){
+                bulls++;
             }else{
-                if(num[the_guess[i] - '0']++ < 0) {
-                        pgia_numbers++;
+                if(num[guess[i] - '0']++ < 0) {
+                        cows++;
                     }
-                if(num[the_choice[i] - '0']-- > 0){
-                        pgia_numbers++;
+                if(num[choice[i] - '0']-- > 0){
+                        cows++;
                     }
                 }
         }
-            return to_string(bull_numbers) + "," +to_string(pgia_numbers);
+            return to_string(bulls) + ", " + to_string(cows);
     }
 }
